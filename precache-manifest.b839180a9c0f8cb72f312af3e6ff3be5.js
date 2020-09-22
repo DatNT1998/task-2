67d7cdfd1bc1d54cb3bd9c8a4a17ab931da018e1
DatNT1998 <44127716+DatNT1998@users.noreply.github.com>
2020-09-22 09:20:33 +0700

    Updates

---
+++ precache-manifest.b839180a9c0f8cb72f312af3e6ff3be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96c5597b078c261e2a8e0a51d928759",
+    "url": "/task-2/index.html"
+  },
+  {
+    "revision": "48ab77bf3da39aa596e1",
+    "url": "/task-2/static/css/main.f3c1ad1b.chunk.css"
+  },
+  {
+    "revision": "dd4a0d1838b3f750d10b",
+    "url": "/task-2/static/js/2.f5f57fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-2/static/js/2.f5f57fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ab77bf3da39aa596e1",
+    "url": "/task-2/static/js/main.a6e34a02.chunk.js"
+  },
+  {
+    "revision": "314c33637e9d091a23e2",
+    "url": "/task-2/static/js/runtime-main.2dcd9a32.js"
+  }
+]);
